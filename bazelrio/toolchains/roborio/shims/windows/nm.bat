4b427386a6febddd9b5edc72fd36e3ef522fb961
Connor Worley <connorbworley@gmail.com>
2021-09-02 21:54:34 -0700

    backslashes

--- bazelrio/toolchains/roborio/shims/windows/nm.bat
+++ bazelrio/toolchains/roborio/shims/windows/nm.bat
@@ -1,1 +1,1 @@
-external/__bazelrio_roborio_toolchain_macos/frc2021/roborio/bin/arm-frc2021-linux-gnueabi-%~n0.exe %*
+external\__bazelrio_roborio_toolchain_macos\frc2021\roborio\bin\arm-frc2021-linux-gnueabi-%~n0.exe %*
