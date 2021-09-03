adc68ef72e7f9cca3ec9945dabc15a735f69d2fa
Connor Worley <connorbworley@gmail.com>
2021-09-02 21:59:24 -0700

    dumb

--- bazelrio/toolchains/roborio/shims/windows/gcc.bat
+++ bazelrio/toolchains/roborio/shims/windows/gcc.bat
@@ -1,2 +1,1 @@
-dir
-external\__bazelrio_roborio_toolchain_macos\frc2021\roborio\bin\arm-frc2021-linux-gnueabi-%~n0.exe %*
+external\__bazelrio_roborio_toolchain_windows\frc2021\roborio\bin\arm-frc2021-linux-gnueabi-%~n0.exe %*
