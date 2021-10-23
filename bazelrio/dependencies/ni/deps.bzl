45f5c11149a9b5c4f6e2d7bc9e69968dc07b834e
PJ Reiniger <pj.reiniger@gmail.com>
2021-10-23 17:40:02 -0400

    Adding beta-1 libraries (#42)
    * Adding java artifacts to deps files
    
    * Adding beta-1 libraries

--- bazelrio/dependencies/ni/deps.bzl
+++ bazelrio/dependencies/ni/deps.bzl
@@ -1,7 +1,10 @@
 load("//dependencies/ni/2020_9_2:deps.bzl", "setup_ni_2020_9_2_dependencies")
+load("//dependencies/ni/2022_2_2:deps.bzl", "setup_ni_2022_2_2_dependencies")
 
 def setup_ni_dependencies(version):
     if version == "2020.9.2":
         setup_ni_2020_9_2_dependencies()
+    elif version == "2022.2.2":
+        setup_ni_2022_2_2_dependencies()
     else:
         fail("Unsupported version '{}'".format(version))
--- scripts/opencv_dependencies.py
+++ scripts/opencv_dependencies.py
@@ -4,18 +4,18 @@ from platforms import default_all_platforms
 
 def opencv_dependencies():
     MAVEN_URL = "https://frcmaven.wpi.edu/release"
-    VERSIONS = ["3.4.7-5"]
+    VERSIONS = [("frc2021", "3.4.7-5"), ("frc2022", "4.5.2-1")]
     DEP_NAME = "opencv"
 
     dependencies = {DEP_NAME: []}
 
-    for version in VERSIONS:
+    for subpackage, version in VERSIONS:
         maven_dep = MavenDependencyGroup(DEP_NAME, MAVEN_URL, version)
         dependencies[DEP_NAME].append(maven_dep)
 
         maven_dep.add_cpp_dep(
             resources=default_all_platforms() + ["headers"],
-            group_id="edu.wpi.first.thirdparty.frc2021.opencv",
+            group_id=f"edu.wpi.first.thirdparty.{subpackage}.opencv",
             artifact_name="opencv-cpp",
         )
     return dependencies
