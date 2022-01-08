efc81692515f5e73514bdce6afe9af7937c77b21
Noam Zaks <imnoamzaks@gmail.com>
2022-01-08 09:35:06 +0200

    update all frc2021 paths

--- bazelrio/toolchains/roborio/shims/windows/ar.bat
+++ bazelrio/toolchains/roborio/shims/windows/ar.bat
@@ -1,2 +1,2 @@
 @echo off
-external\__bazelrio_roborio_toolchain_windows\frc2021\roborio\bin\arm-frc2021-linux-gnueabi-%~n0.exe %*
+external\__bazelrio_roborio_toolchain_windows\frc2022\roborio\bin\arm-frc2022-linux-gnueabi-%~n0.exe %*
